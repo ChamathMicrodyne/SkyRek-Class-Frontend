5c920da675caa834a6e76c565a4b2765be3c4a96
Chama <chamathmicrodyne@gmail.com>
2025-05-07 15:18:55 +0530

    install react-router-dom and setup some pages ✏

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import Header from "./components/header.jsx"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home.jsx";
+import Login from "./pages/login.jsx";
+import Signup from "./pages/signup.jsx";
+import Header from "./components/header.jsx";
 
 function App() {
-
   return (
-    <>
-      <Header/>
-    </>
-  )
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <Routes path="/*">
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/*" element={<h2>404 Not Found!</h2>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return(
         <div>
-            <h1 className="font-bold text-blue-500 text-[50px]">Chama Clothes</h1>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
         </div>
     )
 }
