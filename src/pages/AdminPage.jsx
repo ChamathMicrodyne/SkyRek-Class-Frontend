a81b8ac4422e2a671f970478a1ea5da1cf90ef3b
Chama <chamathmicrodyne@gmail.com>
2025-05-15 12:42:44 +0530

    Make some designs to AdminProductPage and create AddProductPage 🖊

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,22 +4,24 @@ import AdminProductsPage from './admin/AdminProductsPage'
 import AdminUsersPage from './admin/AdminUsersPage'
 import AdminOrdersPage from './admin/AdminOrdersPage'
 import AdminReviewPage from './admin/AdminReviewPage'
+import AddProductPage from './admin/AddProductPage'
 
 function AdminPage() {
   return (
     <div className='w-full h-screen flex'>
-        <div className='w-[300px] h-full bg-green-400 flex flex-col'>
+        <div className='w-[300px] h-full bg-green-200 flex flex-col'>
             <Link to="/admin/products">Products</Link>
             <Link to="/admin/users">Users</Link>
             <Link to="/admin/orders">Orders</Link>
             <Link to="/admin/reviews">Reviews</Link>
         </div>
-        <div className='w-[calc(100%-300px)] h-full bg-amber-200'>
+        <div className='w-[calc(100%-300px)] h-full '>
             <Routes path="/*">
                 <Route path='/products' element={<AdminProductsPage/>}/>
                 <Route path='/users' element={<AdminUsersPage/>}/>
                 <Route path='/orders' element={<AdminOrdersPage/>}/>
                 <Route path='/reviews' element={<AdminReviewPage/>}/>
+                <Route path='/add-product' element={<AddProductPage/>}/>
             </Routes>
         </div>
     </div>
--- src/pages/admin/AdminProductsPage.jsx
+++ src/pages/admin/AdminProductsPage.jsx
@@ -1,52 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import {sampleProducts} from '../../assets/sampleData.js'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { sampleProducts } from "../../assets/sampleData.js";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function AdminProductsPage() {
-  const [products, setProducts] = useState(sampleProducts)
+  const [products, setProducts] = useState(sampleProducts);
 
-  useEffect(()=>{
-    axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products").then((res) => {
-      console.log(res.data)
-    })
-  },[])
-
-  
+  useEffect(() => {
+    axios
+      .get(import.meta.env.VITE_BACKEND_URL + "/api/products")
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
 
   return (
-    <div className='w-full h-full max-h-full bg-blue-200 overflow-hidden overflow-y-scroll'>
-      <table className='w-full text-center'>
-        <thead>
-          <tr>
-            <th>product Id</th>
-            <th>Name</th>
-            <th>Images</th>
-            <th>Labelled Price</th>
-            <th>Price</th>
-            <th>Stock</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            products.map(
-              (item, index) => {
-                return(
-                  <tr key={index}>
-                    <td>{item.productId}</td>
-                    <td>{item.name}</td>
-                    <td><img src={item.images} className='w-[50px] h-[50px]' /></td>
-                    <td>{item.labelledPrice}</td>
-                    <td>{item.price}</td>
-                    <td>{item.stock}</td>
-                  </tr>
-                )
-              }
-            )
-          }
-        </tbody>
-      </table>
+    <div className="w-full h-full max-h-full p-5 bg-[#f4f4f5]">
+      <div className="w-full h-full max-h-full bg-white rounded-xl p-6 shadow-lg">
+        <table className="w-full text-center">
+          <thead>
+            <tr>
+              <th className="pb-[15px]">product Id</th>
+              <th className="pb-[15px]">Name</th>
+              <th className="pb-[15px]">Images</th>
+              <th className="pb-[15px]">Labelled Price</th>
+              <th className="pb-[15px]">Price</th>
+              <th className="pb-[15px]">Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.productId}</td>
+                  <td>{item.name}</td>
+                  <td>
+                    <img src={item.images} className="w-[50px] h-[50px]" />
+                  </td>
+                  <td>{item.labelledPrice}</td>
+                  <td>{item.price}</td>
+                  <td>{item.stock}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Link to="/admin/add-product">
+          <div className="w-[70px] h-[45px] bg-green-400 absolute bottom-10 right-8 flex justify-center items-center border-b-1 border-t-1 rounded-[5px] cursor-pointer">
+            <p className="text-4xl mt-[-8px]">+</p>
+          </div>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AdminProductsPage
+export default AdminProductsPage;
